a05cf7e786808f5ebb744e4db2edf4d41728997f
Oliver Gutiérrez <oliver@alpuca.com>
2014-06-04 10:29:59 +0100

    Added forgotten import

--- sitelog/__init__.py
+++ sitelog/__init__.py
@@ -9,4 +9,7 @@ Website log application
 .. moduleauthor:: (C) 2013 Oliver Gutiérrez
 """
 
+# Django imports
+from django.conf import settings
+
 SITELOG_MAIL_ADMINS_LEVEL = getattr(settings, 'SITELOG_MAIL_ADMINS_LEVEL', 0)
