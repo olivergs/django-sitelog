38c5cddb376648b3f30590af000c4080f47be29c
Oliver Gutiérrez <oliver@alpuca.com>
2014-06-06 09:39:54 +0100

    Fixed content object admin class field to return it's unicode representation instead the object itself

--- sitelog/admin.py
+++ sitelog/admin.py
@@ -23,8 +23,6 @@ class SiteLogInline(GenericTabularInline):
     Site log inline administration class
     """
     model = SiteLog
-    all_fields_readonly=True
-    superuser_skips_all_readonly=False
 
     def has_add_permission(self, request):
         return False
@@ -38,7 +36,6 @@ class SiteLogAdmin(admin.ModelAdmin):
     list_filter = ('timestamp','level','tag','user')
     search_fields = ('message','tag','user__username','user__first_name','user__last_name','ip','data')
     ordering = ('-timestamp',)
-    all_fields_readonly = True
 
     def has_add_permission(self, request):
         return False
@@ -49,7 +46,7 @@ class SiteLogAdmin(admin.ModelAdmin):
         """
         try:
             if obj.content_object:
-                return obj.content_object
+                return unicode(obj.content_object)
         except:
             pass
         return None
